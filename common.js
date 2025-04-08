d67eee1cb7887fedb179bec379eb5d2ced9584a8
gernsdorfer <larswiedemann@gmx.net>
2025-04-08 20:21:12 +0200

    update build

---
+++ common.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkscheibentoenung_schmieder=self.webpackChunkscheibentoenung_schmieder||[]).push([[592],{9224:(m,o,d)=>{d.d(o,{QW:()=>c});var i=d(5e3),n=d(508);let c=(()=>{class e{}return e.\u0275fac=function(a){return new(a||e)},e.\u0275mod=i.oAB({type:e}),e.\u0275inj=i.cJS({imports:[[n.BQ],n.BQ]}),e})()}}]);
